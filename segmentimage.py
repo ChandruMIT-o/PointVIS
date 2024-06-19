dc84a46ff25a9c007e9d8d259778f30c36188efa
iesa04 <146453351+iesa04@users.noreply.github.com>
2024-06-19 21:25:36 +0530

    Updated

--- segmentimage.py
+++ segmentimage.py
@@ -4,6 +4,8 @@ import numpy as np
 import cv2
 import random
 from data_repository.lookup_table import read_table
+import os
+import shutil
 
 ROOT = "C:/Programming/Fourth Semester/PointVIS/PointVIS"
 IMAGE_PATH = ""
@@ -15,11 +17,28 @@ IMAGE_HEIGHT = -1
 
 model = YOLO("projection/yolov8n-seg.pt")
 
+def delete_all_files_and_folders(directory):
+    # Check if the directory exists
+    if os.path.exists(directory):
+        # Iterate over all the files and folders in the directory
+        for filename in os.listdir(directory):
+            file_path = os.path.join(directory, filename)
+            try:
+                # Check if it's a file or directory and delete accordingly
+                if os.path.isfile(file_path) or os.path.islink(file_path):
+                    os.unlink(file_path)  # Remove the file
+                elif os.path.isdir(file_path):
+                    shutil.rmtree(file_path)  # Remove the directory
+            except Exception as e:
+                print(f'Failed to delete {file_path}. Reason: {e}')
+    else:
+        print(f'Directory {directory} does not exist')
+
 def update_path(image_name):
     global IMAGE_PATH, TEXT_FILE_PATH, OUTPUT_PATH
     IMAGE_PATH = ROOT + "/assets/room1/" + image_name
     TEXT_FILE_PATH = (ROOT + "/runs/segment/predict/labels/" + image_name).split(".")[0] + ".txt"
-    OUTPUT_PATH = ROOT + "/assets/room1/" + image_name
+    OUTPUT_PATH = ROOT + "/output/" + image_name
 
 def get_image_size(IMAGE_PATH):
     global IMAGE_WIDTH, IMAGE_HEIGHT
@@ -97,6 +116,8 @@ def draw_segmentations(segmentation_data, width, height):
     return blank_image, color_map
 
 def call_yolo(image_path):
+    # Example usage
+    delete_all_files_and_folders(ROOT + "/runs/segment")
 
     update_path(image_path)
 
@@ -112,4 +133,4 @@ def call_yolo(image_path):
 
     return segmented_image, color_map
 
-call_yolo("view1_frame_1_image_rendered.png")
+call_yolo("room_textured1.jpg")
